ace47303d3163a3ce091fc6231bc5c35668774a2
SametYASIT <120249031+SametYASIT@users.noreply.github.com>
2025-05-03 14:50:35 +0300

    PrivateRouts

--- crypto-app/client/src/components/PrivateRoute.js
+++ crypto-app/client/src/components/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../context/AuthContext';
 
 const PrivateRoute = ({ children }) => {
   const { user } = useAuth();
